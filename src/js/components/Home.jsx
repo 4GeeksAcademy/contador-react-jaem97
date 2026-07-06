cb7a91a770ab15b1f254183923e71c77786cce96
Jesus Echenagucia <jaem0699@gmail.com>
2026-07-06 11:10:29 +0000

    terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,23 +4,57 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+const Home = ({ counter }) => {
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const counterTransform = String(counter).padStart(6, "0");
+	const digitos = counterTransform.split("");
+
+
+	return (
+		<div className="container-fluid bg-black min-vh-100 d-flex justify-content-center align-items-center">
+			<div className="d-flex align-items-center gap-3 p-3 bg-black">
+
+				<div
+					className="d-flex align-items-center justify-content-center rounded-3"
+					style={{
+						width: "70px",
+						height: "100px",
+						backgroundColor: "#111111",
+					}}
+				>
+					
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="40"
+						height="40"
+						fill="currentColor"
+						className="text-white"
+						viewBox="0 0 16 16"
+					>
+						<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+						<path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z" />
+					</svg>
+				</div>
+
+				
+				{digitos.map((digito, index) => (
+					<div
+						key={index}
+						className="text-white d-flex align-items-center justify-content-center rounded-3"
+						style={{
+							width: "70px",
+							height: "100px",
+							backgroundColor: "#111111", 
+							fontSize: "4.5rem",         
+							fontWeight: "500",
+							fontFamily: "'Times New Roman', Times, serif", 
+						}}
+					>
+						{digito}
+					</div>
+				))}
+
+			</div>
 		</div>
 	);
 };
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -11,8 +11,19 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+let counter = 0
+
+
+setInterval(()=>{
+  
+  root.render(
+    <React.StrictMode>
+      <Home counter={counter} />
+    </React.StrictMode>,
+  )
+
+  counter++
+
+}, 1000)
